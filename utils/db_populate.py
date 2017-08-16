302fe568999b461f4383b8a0c12fd6e7af9b9664
Jakub Czapliński <sirupgrade@gmail.com>
2017-08-16 16:00:15 +0200

    Refaktoring.

--- utils/db_populate.py
+++ utils/db_populate.py
@@ -1,4 +1,4 @@
 from superweb.database import Task
 
 task_item = Task(title='Zrób PR na GitHubie', description='Umieść pozdrowienia dla @upgrade i opisz jak bardzo chesz aby twój PR otrzymał merga. XOXO')
-task_item.save() # bob is now stored in the database
+task_item.save()
