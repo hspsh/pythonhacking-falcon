7ede00ea36b12e25367d782bb36e0d7ced7e1870
Łukasz Rogalski <rogalski.91@gmail.com>
2017-09-13 23:42:52 +0200

    use Faker for db_*.py utils

--- utils/db_populate.py
+++ utils/db_populate.py
@@ -1,5 +1,19 @@
+import random
+
+import faker
+
 from superweb import database
 
-task_item = database.Task(title='Zrób PR na GitHubie',
-                 description='Umieść pozdrowienia dla @upgrade i opisz jak bardzo chesz aby twój PR otrzymał merga. XOXO')
-task_item.save()
+fake = faker.Faker()
+
+for x in range(50):
+    is_completed = random.random() < 0.25
+    task_item = database.Task(
+        title=fake.bs(),
+        description=' '.join(fake.sentences(3)),
+        is_completed=is_completed,
+        created_at=fake.date_time_this_year(),
+        completed_at= fake.date_time_this_year() if is_completed else None,
+        deadline_at=fake.date_time_this_year(before_now=True, after_now=True) if random.random() < 0.75 else None
+    )
+    task_item.save()
