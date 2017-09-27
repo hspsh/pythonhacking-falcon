fc35376952ff2f408708b2790ca67a8333007cd1
Łukasz Rogalski <rogalski.91@gmail.com>
2017-09-27 19:07:26 +0200

    Rest API for TodoTasks

--- superweb/main.py
+++ superweb/main.py
@@ -2,24 +2,18 @@ from wsgiref.simple_server import make_server
 
 import falcon
 
-from superweb.resources import ToDo, JsonHello, ToDoList, ToDoJsonList, ToDoComplete, ToDoTaskCompleted, Index
+from superweb import resources
 
 app = falcon.API()
 
-todo = ToDo()
-json_hello = JsonHello()
-todo_list = ToDoList()
-todo_complete = ToDoComplete()
-todo_task_completed = ToDoTaskCompleted()
-json_todo_list = ToDoJsonList()
-
-app.add_route('/', Index())
-app.add_route('/todo/add', todo)
-app.add_route('/todo/complete/{task_id}', todo_complete)
-app.add_route('/todo/completed', todo_task_completed)
-app.add_route('/todo/', todo_list)
-app.add_route('/json_hello', json_hello)
-app.add_route('/json_todo_list', json_todo_list)
+app.add_route('/', resources.Index())
+app.add_route('/todo/add', resources.ToDo())
+app.add_route('/todo/complete/{task_id}', resources.ToDoComplete())
+app.add_route('/todo/completed', resources.ToDoTaskCompleted())
+app.add_route('/todo/', resources.ToDoList())
+app.add_route('/json_hello', resources.JsonHello())
+app.add_route('/api/todo_tasks/{task_id}', resources.TodoTaskResource())
+app.add_route('/api/todo_tasks', resources.TodoTasksResource())
 
 if __name__ == '__main__':
     httpd = make_server('', 8000, app)
